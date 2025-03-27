4a918d7620a34f9948c31a4b4002014248cdf8c2
ZCHUA040 <159976032+ZCHUA040@users.noreply.github.com>
2025-03-27 12:53:33 +0800

    remove session schema

--- backend/app.py
+++ backend/app.py
@@ -21,7 +21,7 @@ from flask_mail import Mail
 #DB
 from control import recovery_account_controller
 from control import user_controller
-from entity.models import db, User, Session
+from entity.models import db, User
 
 # Flask app setup https://blog.miguelgrinberg.com/post/how-to-create-a-react--flask-project
 app = Flask(__name__)
--- backend/entity/models.py
+++ backend/entity/models.py
@@ -8,11 +8,3 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
-
-class Session(db.Model):
-    __tablename__ = 'Session'  # Match table name in SQLite
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
-    token = db.Column(db.String(255), unique=True, nullable=False)
-    expires_at = db.Column(db.Text, nullable=False)  # TEXT for timestamp
-    
